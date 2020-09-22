947d3af66d428712f05a11acf03daab2dd4ac0b9
luismanez <luis.manez@outlook.com>
2020-09-22 15:04:29 +0200

    Auth added to website project using Microsoft.Identity.Web libraries

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # netCore Conference Virtual 2 (3 October 2020)
 Materials from my session [__Azure AD MFA + Conditional Acess for Developers__](https://virtual-netcoreconf-2.sessionize.com/session/209572) for the netCoreConf 2 Virtual 2020.
+
+## Deploy
+Update AppSettings.json with your Azure AD Apps values (ClientId, Secret...)
--- src/Delos.Westworld.Website/Startup.cs
+++ src/Delos.Westworld.Website/Startup.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
 
 namespace Delos.Westworld.Website
 {
@@ -23,7 +28,26 @@ namespace Delos.Westworld.Website
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.Unspecified;
+                // Handling SameSite cookie according to https://docs.microsoft.com/en-us/aspnet/core/security/samesite?view=aspnetcore-3.1
+                options.HandleSameSiteCookieCompatibility();
+            });
+
+            services.AddOptions();
+
+            services.AddMicrosoftIdentityWebAppAuthentication(Configuration);
+
+            services.AddControllersWithViews(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            }).AddMicrosoftIdentityUI();;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +68,7 @@ namespace Delos.Westworld.Website
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
